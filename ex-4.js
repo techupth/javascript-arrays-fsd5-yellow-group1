a77b79856b58199ddb96fe752c24a0c95188bfc2
Jing <phuttipong.cu@gmail.com>
2023-11-29 00:54:55 +0700

    JS-assignment

--- ex-4.js
+++ ex-4.js
@@ -11,4 +11,6 @@ const employees = [
   },
 ];
 
-// Start coding here
+// Start coding here: 
+employees.push({name: "Alicia",age: 29,hobbies: ["Shopping","Reading novels"]},{name: "Kody",age: 19,hobbies: ["Computer games","Wakeboard"]})
+console.log(employees)
